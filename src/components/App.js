b09d9be712f74af51550958c5e8bc9810bc2c6b3
caiohbe <caiohbe@gmail.com>
2022-09-27 13:48:09 -0300

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+import Cards from "./Cards";
+import Footer from "./Footer";
+
+export default function App() {
+    return (
+        <React.Fragment>
+            <Header />
+            <Cards />
+            <Footer />
+        </React.Fragment>
+    )
+}
