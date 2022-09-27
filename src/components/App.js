eee6216cc71bf27e5f7532d81da411b20a8e3b6a
caiohbe <caiohbe@gmail.com>
2022-09-27 14:49:33 -0300

    Header

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,23 @@ import React from "react";
 import Header from "./Header";
 import Cards from "./Cards";
 import Footer from "./Footer";
+import styled from "styled-components";
 
 export default function App() {
     return (
-        <React.Fragment>
+        <Container>
             <Header />
             <Cards />
             <Footer />
-        </React.Fragment>
+        </Container>
     )
 }
+
+const Container = styled.div`
+    background-color: #FB6B6B;
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,29 @@
+import React from "react"
+import ZapLogo from "../assets/img/logo.png"
+import styled from "styled-components"
+
 export default function Header() {
     return (
-        <div>Header js</div>
+        <Logo>
+            <img src={ZapLogo} alt="Zap" />
+            ZapRecall
+        </Logo>
     )
 }
+
+const Logo = styled.div`
+    color: white;
+    font-size: 36px;
+    line-height: 45px;
+    letter-spacing: -0.012em;
+    text-align: center;
+    display: flex;
+    align-items: center;
+    font-family: 'Righteous', cursive;
+    margin-top: 50px;
+    
+    img {
+        margin-right: 18px;
+    }
+
+`
