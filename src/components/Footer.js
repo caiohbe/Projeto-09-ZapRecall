3115a53b098271f561ee6b6b6c982537a292aac3
caiohbe <caiohbe@gmail.com>
2022-09-30 16:34:49 -0300

    proper order on progress

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,46 @@
 import styled from "styled-components"
+import incorrectIcon from "../assets/img/icone_erro.png"
+import almostIcon from "../assets/img/icone_quase.png"
+import correctIcon from "../assets/img/icone_certo.png"
+import React from "react"
 
 export default function Footer(props) {
+    let arr = ['','','','','','','','']
     return (
         <Bottom>
-            {props.done.length/2}/8 CONCLUÍDOS
+            <h3>{props.done.length/2}/8 CONCLUÍDO</h3>
+            <div>
+                <Icon done={props.done}/>
+            </div>
         </Bottom>
     )
+    
+    function Icon(props) {
+        props.done.forEach((e, i) => {
+            if (i % 2 !== 0) {
+                arr[props.done[i-1]] = e
+            }
+        }) 
+
+        return arr.map((e) => {
+            switch (e) {
+                case 'incorrect':
+                    return <img src={incorrectIcon} />
+                    break;
+                
+                case 'almost':
+                    return <img src={almostIcon} />
+                    break
+                
+                case 'correct':
+                    return <img src={correctIcon} />
+                    break
+
+                default:
+                    break;
+            }
+        })
+    }
 }
 
 const Bottom = styled.div`
@@ -21,4 +56,12 @@ const Bottom = styled.div`
     font-size: 18px;
     font-weight: 400;
     line-height: 22px;
+    display: flex;
+    flex-direction: column;
+
+    img {
+    padding: 2px;
+    } 
+
+
 `
